9b4f47513e0b8e9cab0f0a0b407a7298b96a61a0
Balasubramanian <2018pgcaca23@nitjsr.ac.in>
2020-07-12 08:23:15 +0530

    Create Readme.m

---
+++ Python/Human Activity Recognition System using Tensorflow/Readme.m
@@ -0,0 +1,13 @@
+# HUMAN ACTIVITY RECOGNITION SYSTEM USING TENSORFLOW AND CNN
+
+Tools Required
+Python 3 is used during development and following libraries are required to run the code provided in the notebook:
+
+Tensorflow
+Numpy
+Matplotlib
+Pandas
+
+### Dataset
+The WISDM Actitracker dataset used for model training, can be downloaded from the following link:
+http://www.cis.fordham.edu/wisdm/dataset.php
